cabdc311b381c861635ffb67fb949aa72fdb1aeb
OOP J <arthawebid@gmail.com>
2025-03-25 21:59:51 +0800

    Pertemuan4update

--- Pertemuan401/src/pertemuan401/Pertemuan401.java
+++ Pertemuan401/src/pertemuan401/Pertemuan401.java
@@ -1,5 +1,7 @@
 package pertemuan401;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Made Artha
@@ -11,6 +13,21 @@ public class Pertemuan401 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        String[] nama = new String[5];
+        
+        for(int y=0;y<5;y++){
+            System.out.print("isi Data Teman: ");
+            nama[y] = inp.nextLine();
+        }
+        
+        for(int i=0;i<nama.length;i++){
+            System.out.println("Data ke-"+(i+1)+": "+nama[i] );
+        }
+        
+        for(String x : nama){
+            System.out.println(x);
+        }
         
     }
     
